f84de9bf427189190bea95b03e027757f7a33f85
uzleo <uzleo_eth@proton.me>
2026-04-26 20:03:27 +0000

    feat: add a helpful printer for AST

--- example/lexer/foo.sv
+++ example/lexer/foo.sv
@@ -1,6 +1,13 @@
 
-
-
-module foo #(parameter int N = 8) ();
-  wire [N-1 : 0] bus;
+module foo #(
+  parameter int N = 8,
+  parameter logic [7:0] MASK = N - 1, ALT_MASK = 2,
+  parameter type T = logic [3:0], U = bit
+) (
+  input clk,
+  input rst_n,
+  output logic [N-1 : 0] bus,
+  valid
+);
+  wire [N-1 : 0] internal_bus;
 endmodule
